e9805b5a5e22bb56978e8a085faec2da3ff47d3c
LBY-L <srgato.azul@gmail.com>
2025-04-28 21:58:49 -0600

    I deleted the setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+setup(
+    name="termc",
+    version="v0.9.2",
+    author="LBY_L",
+    license="GNU General Public License 3.0 (GPL 3.0)",
+    description="A simple script to download and execute Minecraft servers",
+    packages=find_packages(),
+    install_requires=[
+        "yaspin",
+        "InquirerPy",
+        "psutil"
+    ],
+    keywords=["cli", "tui"],
+    entry_points={"console_scripts": "termc = termc.termc:Init"},
+    python_requires=">=3.13",
+)
